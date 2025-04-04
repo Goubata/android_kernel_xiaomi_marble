a06176e80ed5d91921200d98bf69dd2652d93696
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-04-05 01:44:19 +0900

    Update governor_msm_adreno_tz.c

--- drivers/gpu/msm/governor_msm_adreno_tz.c
+++ drivers/gpu/msm/governor_msm_adreno_tz.c
@@ -613,6 +613,8 @@ static int tz_start(struct devfreq *devfreq)
 	ret = __tz_init(devfreq);
 	if (ret)
 		return ret;
+		
+		priv = (struct devfreq_msm_adreno_tz_data *)devfreq->data;
 
 	for (i = 0; adreno_tz_attr_list[i] != NULL; i++)
 		device_create_file(&devfreq->dev, adreno_tz_attr_list[i]);
