d825fc579fe1ee9e121b812e827fbc633fe591ff
Rakkashin <101036342+Goubata@users.noreply.github.com>
2025-04-05 19:55:21 +0900

    Update msm_adreno_devfreq.h

--- drivers/gpu/msm/msm_adreno_devfreq.h
+++ drivers/gpu/msm/msm_adreno_devfreq.h
@@ -54,6 +54,7 @@ struct devfreq_msm_adreno_tz_data {
 	unsigned int device_id;
 	bool is_64;
 	bool boost_enabled;
+	int boost_level;
 	bool disable_busy_time_burst;
 	bool ctxt_aware_enable;
 	/* Multiplier to change gpu busy status */
